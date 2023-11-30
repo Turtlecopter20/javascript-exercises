9150d8dd38fddb7c97a524bbb26f60443eb78312
Turtlecopter20 <dalex00.da00@gmail.com>
2023-11-30 21:26:43 +0200

    Finish third js repo exercise with all the tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
-
+//Declare variable that will be the word given
+let word = "";
+const reverseString = function(word) {
+let middlestep = [];
+//Split every character from that input into an array containing where every character is allocated a new index
+for (let i= 0; i < word.length; i++) {
+    middlestep[i] = word.charAt(i);
+}
+let returned="";
+//Declare another variable that will be the string which will be the reverse input and iterate until the number of characters matches the first string
+for (let j = word.length-1; j >= 0; j--){
+ returned += middlestep[j];
+}
+return returned;
 };
 
 // Do not edit below this line
