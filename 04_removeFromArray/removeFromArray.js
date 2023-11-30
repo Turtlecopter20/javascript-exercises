b10552b87536d14d627986d3851c5edbabd4d7b6
Turtlecopter20 <dalex00.da00@gmail.com>
2023-11-30 22:13:12 +0200

    Finish fourth js exercise with first test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(theArray, toSubstract) {
+for (let i = 0; i < theArray.length; i++){
+if (theArray[i] == toSubstract) {
+    theArray.splice(i,1);
+}
+}
+return theArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
