daf2b73d8d86fcd324163a09c179d2711f01537f
Turtlecopter20 <dalex00.da00@gmail.com>
2023-12-02 20:54:52 +0200

    Finish 6th exercise from js repo with tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(checkedYear) {
+if (checkedYear % 4 === 0) {
+    if (checkedYear % 100 === 0 && checkedYear % 400 !== 0) return false;
+    else return true;
+}
+else return false;
 };
 
 // Do not edit below this line
